90069052451ce822acd597b33ce6c43023584900
shun senoo <shunsenoobb@gmail.com>
2020-03-04 10:35:31 +0900

    Initial Commit

---
+++ SampleMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleMemo
+//
+//  Created by 妹尾駿 on 2020/03/04.
+//  Copyright © 2020 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
